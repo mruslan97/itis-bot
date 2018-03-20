396a32889a8ba7d46fd796d202aa191ea8570b1e
Ruslan Murtazin <rouslanmurtazin@yandex.ru>
2018-03-20 11:36:32 +0300

    initial parsers

---
+++ MegaParser/Parsers/SeminarParser.cs
@@ -0,0 +1,58 @@
+﻿using MegaParser.Models;
+
+namespace MegaParser.Parsers
+{
+    public class SeminarParser : IParser
+    {
+        public ParsedSubject Parse(string input)
+        {
+            var initialsCounter = 0;
+            var parsedSubject = new ParsedSubject
+            {
+                Cabinet = "",
+                Notation = "",
+                SubjectName = "",
+                Teacher = "",
+                Time = ""
+            };
+            var notationCheck = false;
+            var upperCaseCheck = false;
+            var i = 0;
+            foreach (var char_ in input)
+            {
+                i++;
+                if (char_.Equals('(')) notationCheck = true;
+                if (i > 1)
+                    if (char.IsUpper(char_))
+                        upperCaseCheck = true;
+                if (char.IsNumber(char_) && parsedSubject.Cabinet.Length < 4 && notationCheck == false
+                ) 
+                {
+                    parsedSubject.Cabinet = parsedSubject.Cabinet + char_;
+                }
+                else
+                {
+                    if (!upperCaseCheck)
+                    {
+                        parsedSubject.SubjectName = parsedSubject.SubjectName + char_;
+                    }
+                    if (notationCheck)
+                    {
+                        parsedSubject.Notation = parsedSubject.Notation + char_;
+                    }
+                    if (initialsCounter < 3)
+                    {
+                        parsedSubject.Teacher = parsedSubject.Teacher + char_;
+                        if (char.IsUpper(char_))
+                            initialsCounter++;
+                    }
+                }
+
+                if (char_.Equals(')'))
+                    notationCheck = false;
+            }
+            parsedSubject.Teacher += ".";
+            return parsedSubject;
+        }
+    }
+}
