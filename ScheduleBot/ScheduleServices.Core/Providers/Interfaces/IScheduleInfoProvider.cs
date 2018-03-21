4b88290734167b8a33b85d9f73e1f69c956e533d
Oleg Shatin <mongoose0911@gmail.com>
2018-03-21 07:38:34 +0300

    provider interface added

---
+++ ScheduleBot/ScheduleServices.Core/Providers/Interfaces/IScheduleInfoProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using ScheduleServices.Core.Models.Interfaces;
+
+namespace ScheduleServices.Core.Providers.Interfaces
+{
+    public interface IScheduleInfoProvider
+    {
+        Task<ISchedule> GetScheduleAsync(IScheduleGroup group, DayOfWeek day);
+    }
+}
