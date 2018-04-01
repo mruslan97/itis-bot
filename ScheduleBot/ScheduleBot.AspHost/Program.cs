6759ff861a97e7054a1d0b07e2f637c7c2aa4c90
Ruslan Murtazin <rouslanmurtazin@yandex.ru>
2018-04-01 23:54:43 +0300

    clean main lol

--- ScheduleBot/ScheduleBot.AspHost/Program.cs
+++ ScheduleBot/ScheduleBot.AspHost/Program.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Linq;
-using Microsoft.AspNetCore;
+﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using ScheduleServices.Core.Models.Interfaces;
-using ScheduleServices.Core.Models.ScheduleGroups;
 
 namespace ScheduleBot.AspHost
 {
@@ -21,31 +7,16 @@ namespace ScheduleBot.AspHost
     {
         public static void Main(string[] args)
         {
-            //var testGroup = new ScheduleGroup {GType = ScheduleGroupType.Academic, Name = "11-507"};
-            //var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\BotStorage\\" + "usersgroups.xml";
-            //var xdoc = XDocument.Load(path);
-            //Console.WriteLine(testGroup.GType.ToString());
-            //var element = xdoc.Element("users")
-            //    ?.Elements("user").Where(u =>
-            //        u.Element("chatId")?.Value == "259862692" &&
-            //        u.Element("groupType")?.Value == testGroup.GType.ToString());
-
-            //xdoc.Element("users")
-            //    .Add(new XElement("user",
-            //        new XAttribute("name", "ruslan"),
-            //        new XElement("groupType", testGroup.GType.ToString()),
-            //        new XElement("chatId", "259862692"),
-            //        new XElement("value", testGroup.Name)));
-            //xdoc.Save(path);
-            //Console.ReadKey();
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            new WebHostBuilder()
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseKestrel()
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
