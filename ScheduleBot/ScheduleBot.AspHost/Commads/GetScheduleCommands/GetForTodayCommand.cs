86348635fb775cfcb299db8e32ca6d78f966e6ac
Oleg Shatin <mongoose0911@gmail.com>
2018-04-01 15:26:05 +0300

    fortoday check implemented

--- ScheduleBot/ScheduleBot.AspHost/Commads/GetScheduleCommands/GetForTodayCommand.cs
+++ ScheduleBot/ScheduleBot.AspHost/Commads/GetScheduleCommands/GetForTodayCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ScheduleBot.AspHost.BotStorage;
@@ -25,8 +26,7 @@ namespace ScheduleBot.AspHost.Commads.GetScheduleCommands
         {
             if (!base.CanHandleCommand(update))
             {
-                //todo: impl
-                return false;
+                return update.Message.Text.ToLowerInvariant().Contains("сегодня");
             }
             else
                 return true;
